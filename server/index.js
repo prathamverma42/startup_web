8eaff7ca953ff8a97ff715822618e3b6d31603a9
Pratham Verma <prathamverma12345@gmail.com>
2021-04-15 09:22:44 +0530

    config file added

--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import BodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
 import dashboardRoute from './routers/dashboard.js';
 import homeRoute from './routers/home.js';
+
+dotenv.config({path: './config/config.env'});
+
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(BodyParser.json());
 app.use(cors());
 app.use('/',dashboardRoute);
 app.use('/v1/home',homeRoute);
 
-const MONGO_URI="mongodb+srv://pverma42:prathamverma26142@cluster1.mkm8i.mongodb.net/test";
-const PORT = 5000 || 3000;
 
-mongoose.connect(MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI,{
     useFindAndModify:false,
     useNewUrlParser: true,
     useUnifiedTopology:true,
